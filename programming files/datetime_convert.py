c2516cdb9ab22b9553ab584168921feded0bf290
abarut55 <112417990+abarut55@users.noreply.github.com>
2023-03-21 10:58:58 -0400

    Update datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -3,6 +3,6 @@ from datetime import datetime
 date_str = "2022-03-17 10:45:30"
 date_ob = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 # Uses variable name that doesn't exist 
-formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
+formatted_date = date_ob.strftime('%m/%d/%Y %H:%M:%S')
 
 print(formatted_date)
